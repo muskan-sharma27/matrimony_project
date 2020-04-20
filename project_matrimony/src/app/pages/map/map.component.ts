c8dc6a0a50cfca52e555cc53b0db6ba104365c7f
Dheshoju Kalyan Kumar <kalyanspade.n@gmail.com>
2020-04-20 14:43:43 +0530

    Base code

---
+++ project_matrimony/src/app/pages/map/map.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from "@angular/core";
+
+declare const google: any;
+
+interface Marker {
+lat: number;
+lng: number;
+label?: string;
+draggable?: boolean;
+}
+
+@Component({
+  selector: "app-map",
+  templateUrl: "map.component.html"
+})
+export class MapComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {
+
+        
+  }
+}
